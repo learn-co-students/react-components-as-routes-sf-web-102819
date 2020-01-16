f023e07ab2569aa1304f08e06d516a9c470b5a3b
Your_Name <joseph161216@gmail.com>
2020-01-15 16:50:34 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,133 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from "./components/Home"
+import About from "./components/About"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar"
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const Home = () => {
+/* Add basic styling for NavLinks */
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
+
+/* add the navbar component */
+// const Navbar = () =>
+//   <div>
+//     <NavLink
+//       to="/"
+//       /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//       exact
+//       /* add styling to Navlink */
+//       style={link}
+//       /* add prop for activeStyle */
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Home</NavLink>
+//     <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >About</NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Login</NavLink>
+//     <NavLink
+//       to="/signup"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >SignUp</NavLink>
+//     <NavLink
+//       to="/messages"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Messages</NavLink>
+//   </div>;
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   )
+// }
+
+// ********************* Custom created methods *********************
+const Signup = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>Please Sign Up!</h1>
     </div>
-  );
-};
+  )
+}
 
+const Messages = () => {
+  return (
+    <div>
+      <h1>Your messages!</h1>
+    </div>
+  )
+}
+// ********************* Custom created methods *********************
+
+// Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <divt>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
+    </divt>
+  </Router>,
   document.getElementById('root')
 );
